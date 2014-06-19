e33e16586d46fc169760acd0f1fcbebcb5711c01
Luke Zhao <luke.zhao@polar.me>
2014-06-19 15:00:51 -0400

    delete scheduler

--- public/uwscheduler/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'config.php';
-
-header('Content-type: text/html; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-
-echo Dispatcher::dispatch($_SERVER['REQUEST_URI']);
-?>
