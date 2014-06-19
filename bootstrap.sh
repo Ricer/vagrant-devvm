127252211b5158a7ea62b1b84e978b44ed8cb095
Luke Zhao <luke.zhao@polar.me>
2014-06-19 14:32:48 -0400

    add uwscheduler

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+
+uwcarpool_dump = /var/www/dump.sql
+uwschedule_dump = /var/www/dump.sql
+
+#create databases and users  
 echo "creating UWCarpool User"
 mysql -u root --password="root" -h localhost < /vagrant/user.sql
+
+
+#import dump
 echo "importing SQL dump"
-mysql -u root --password="root" -h localhost carpoolfinder < /var/www/dump.sql
+mysql -u root --password="root" -h localhost carpoolfinder < $uwcarpool_dump
+mysql -u root --password="root" -h localhost carpoolfinder < $uwschedule_dump
--- user.sql
+++ user.sql
@@ -1,3 +1,9 @@
+# UWCarpool database and user
 CREATE USER 'uwcarpool'@'localhost' IDENTIFIED BY 'Thisispassword';
 CREATE DATABASE carpoolfinder;
 GRANT ALL ON carpoolfinder.* TO 'uwcarpool'@'localhost' IDENTIFIED BY 'Thisispassword';
+
+# UWScheduler database and user
+CREATE USER 'uwschedule'@'localhost' IDENTIFIED BY 'Thisispassword';
+CREATE DATABASE scheduler;
+GRANT ALL ON scheduler.* TO 'uwschedule'@'localhost' IDENTIFIED BY 'Thisispassword';
